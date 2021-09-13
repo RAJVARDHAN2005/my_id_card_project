657083c8f9ff53e16768f38e805a4278883b3c65
RAJVARDHAN2005 <54369779+RAJVARDHAN2005@users.noreply.github.com>
2021-09-13 19:37:58 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,15 @@ void main() => runApp(MaterialApp(
       home: my_id(),
     ));
 
-class my_id extends StatelessWidget {
+class my_id extends StatefulWidget {
+  @override
+  _my_idState createState() => _my_idState();
+}
+
+class _my_idState extends State<my_id> {
+  int ageIncrement = 16;
+  int gradeIncrement = 11;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,13 +72,60 @@ class my_id extends StatelessWidget {
                 height: 5.0,
               ),
               Text(
-                '16',
+                '$ageIncrement',
                 style: TextStyle(
                     color: Colors.yellow,
                     fontWeight: FontWeight.bold,
                     letterSpacing: 2.0,
                     fontSize: 20),
               ),
+              SizedBox(
+                height: 8.0,
+              ),
+              Row(children: [
+                FloatingActionButton(
+                    backgroundColor: Colors.grey[600],
+                    child: Text(
+                      'Age +',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        ageIncrement += 1;
+                      });
+                    }),
+                SizedBox(
+                  width: 50.0,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.grey[600],
+                    child: Text(
+                      'Age -',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        ageIncrement -= 1;
+                        if (ageIncrement < 1) {
+                          ageIncrement = 1;
+                        }
+                      });
+                    }),
+                SizedBox(
+                  width: 50.0,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.grey[600],
+                    child: Text(
+                      'Clear',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        ageIncrement = 16;
+                      });
+                    }),
+                SizedBox(
+                  width: 50.0,
+                ),
+              ]),
               SizedBox(
                 height: 30.0,
               ),
@@ -87,7 +142,7 @@ class my_id extends StatelessWidget {
                 height: 5.0,
               ),
               Text(
-                '11TH GRADE',
+                '$gradeIncrement GRADE',
                 style: TextStyle(
                     color: Colors.yellow,
                     fontWeight: FontWeight.bold,
@@ -95,28 +150,52 @@ class my_id extends StatelessWidget {
                     fontSize: 20),
               ),
               SizedBox(
-                height: 30.0,
+                height: 8.0,
               ),
-              Text(
-                'INSTITUTION',
-                style: TextStyle(
-                    color: Colors.grey[200],
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 1.2,
-                    fontSize: 16),
-                // height
-              ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Text(
-                'Little Flowers Junior College',
-                style: TextStyle(
-                    color: Colors.yellow,
-                    fontWeight: FontWeight.bold,
-                    letterSpacing: 2.0,
-                    fontSize: 20),
-              ),
+              Row(children: [
+                FloatingActionButton(
+                    backgroundColor: Colors.grey[600],
+                    child: Text(
+                      'Grade+',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        gradeIncrement += 1;
+                      });
+                    }),
+                SizedBox(
+                  width: 50.0,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.grey[600],
+                    child: Text(
+                      'Grade-',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        gradeIncrement -= 1;
+                        if (gradeIncrement < 1) {
+                          gradeIncrement = 1;
+                        }
+                      });
+                    }),
+                SizedBox(
+                  width: 50.0,
+                ),
+                FloatingActionButton(
+                    backgroundColor: Colors.grey[600],
+                    child: Text(
+                      'Clear',
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        gradeIncrement = 11;
+                      });
+                    }),
+                SizedBox(
+                  width: 50.0,
+                ),
+              ]),
               SizedBox(
                 height: 30.0,
               ),
